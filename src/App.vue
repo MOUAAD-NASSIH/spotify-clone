3c240a0969ce7e8c59d8f7a19883f5af8ece814c
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2023-08-23 14:44:43 +0100

    Feature: Adding the Header + SideNav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import MenuItem from "@/components/MenuItem.vue";
 // icons
 import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
 import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
@@ -12,6 +13,88 @@ let openMenu = ref(false);
 
 <template>
   <div>
-    
+    <div
+      class="w-[calc(100%-240px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
+    >
+      <div class="flex items-center ml-6">
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] cursor-pointer"
+        >
+          <ChevronLeft fillColor="#FFFFFF" :size="30" />
+        </button>
+        <button
+          type="button"
+          class="rounded-full bg-black p-[1px] ml-4 cursor-pointer"
+        >
+          <ChevronRight fillColor="#FFFFFF" :size="30" />
+        </button>
+      </div>
+      <button
+        @click="openMenu = !openMenu"
+        :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+      >
+        <div class="flex items-center">
+          <img
+            class="rounded-full"
+            width="27"
+            src="https://yt3.ggpht.com/yti/AOXPAcWwBykmg7LZi3jWVzoH1E1D9BIGwO_3rkQ02VuE4A=s88-c-k-c0x00ffffff-no-rj"
+            alt="pfp"
+          />
+          <div class="text-white text-[14px] ml-1.5 font-semibold">
+            Yusha Dev
+          </div>
+          <ChevronDown
+            v-if="!openMenu"
+            @click="openMenu = true"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+          <ChevronUp
+            v-else="openMenu"
+            @click="openMenu = false"
+            fillColor="#FFFFFF"
+            :size="25"
+          />
+        </div>
+      </button>
+      <span
+        v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-md top-[52px] right-[35px] p-1 cursor-pointer"
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">
+            Profile
+          </li>
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">Log out</li>
+        </ul>
+      </span>
+    </div>
+
+    <!-- sideNav -->
+    <div id="sideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black">
+      <RouterLink to="/">
+        <img width="125" src="/public/images/icons/spotify-logo.png" alt="" />
+      </RouterLink>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLink to="/">
+          <MenuItem class="ml-[1px]" :iconSize="23" name="Home" iconString="home" pageUrl="/" />
+        </RouterLink>
+        <RouterLink to="/search">
+          <MenuItem class="ml-[1px]" :iconSize="24" name="Search" iconString="search" pageUrl="/search" />
+        </RouterLink>
+        <RouterLink to="/library">
+          <MenuItem class="ml-[2px]" :iconSize="23" name="Library" iconString="library" pageUrl="/library" />
+        </RouterLink>
+        <div class="py-3.5"></div>
+        <MenuItem class="ml-[1px]" :iconSize="23" name="Create Playlist" iconString="playlist" pageUrl="/playlist" />
+        <MenuItem class="ml-[1px]" :iconSize="23" name="Liked Songs" iconString="liked" pageUrl="/liked" />
+      </ul>
+
+      <div class="border-b border-b-gray-700"></div>
+
+    </div>
   </div>
 </template>
