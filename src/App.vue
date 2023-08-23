d12460febe7d2dc3dbdcc79de282e58aecd92f68
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2023-08-23 08:41:57 +0100

    scaffolding: initiate the app and installing packages.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+// icons
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+
+let openMenu = ref(false);
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
